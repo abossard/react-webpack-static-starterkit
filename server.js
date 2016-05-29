3b62948d336d1c8effbfe1ceef42a53d3ed9c3e2
Andre Bossard <abossard@gmail.com>
2016-05-29 09:16:10 +0200

    fantastic

--- server.js
+++ server.js
@@ -1,5 +1,2 @@
 require('babel-register')
-console.log(1)
 const server = require('./src/server.js').default
-console.log(2, server)
-server()
--- src/server.js
+++ src/server.js
@@ -1,50 +1,74 @@
-const fs = require('fs')
-const Webpack = require('webpack')
-const webpackConfig = require('../webpack.config.js')
-const path = require('path')
-const debug = require('debug')
-const express = require('express')
-const renderToString = require('react-dom/server').renderToString
+import fs from 'fs'
+import Webpack from 'webpack'
+import webpackConfig from '../webpack.config.js'
+import path from 'path'
+import debug from 'debug'
+import express from 'express'
+import {renderToString, renderToStaticMarkup} from 'react-dom/server'
 
 const log = debug('SERVER')
 
-const React = require('react')
-const ReactRouter = require('react-router')
-const match = ReactRouter.match
-const RouterContext = ReactRouter.RouterContext
-const Route = ReactRouter.Route
-const Link = ReactRouter.Link
-const Router = ReactRouter.Router
+import React from 'react'
+import {match, RouterContext, Route, IndexRoute, Router, Link} from 'react-router'
 
-const App = function (props, context) {
-    return <div>HI</div>
+const App = function (props) {
+    return <div>APP</div>
 }
-log(99)
-const routes = <Route path="/" component={App}/>
+
+const Home = function (props) {
+    return <div>HOME</div>
+}
+const Index = function (props) {
+    return <html>
+        <head>
+            <title>{props.title}</title>
+        </head>
+        <body>
+            <div className='content' dangerouslySetInnerHTML={{ __html: props.html }}/>
+        </body>
+    </html>
+}
+const renderPage = function (renderProps, isRenderStatic = false) {
+    const routerContext = <RouterContext {...renderProps}/>
+    return renderToStaticMarkup(<Index title={getTitleForPath(renderProps.location.pathname) } html={ (isRenderStatic ? renderToStaticMarkup : renderToString)(routerContext) }/>)
+}
+
+const getTitleForPath = function (path) {
+    return `Path: ${path}`
+}
+const routes2 = <Route component={App}>
+    <IndexRoute component={Home}/>
+</Route>
+
+const routes = {
+    path: '/',
+    component: App
+}
 
 const app = express()
-export default function () {
-    app.use('/', function (req, res) {
-        log('Sending ', req)
-        match({ routes, location: req.url }, (error, redirectLocation, renderProps) => {
-            if (error) {
-                res.status(500).send(error.message)
-            } else if (redirectLocation) {
-                res.redirect(302, redirectLocation.pathname + redirectLocation.search)
-            } else if (renderProps) {
-                res.status(200).send(renderToString(<RouterContext {...renderProps} />))
-            } else {
-                res.status(404).send('Not found')
-            }
-        })
-    });
-    log(99)
-    app.listen(process.env.PORT || 3000, 'localhost', function (err, result) {
-        if (err) {
-            console.error(err);
-            log(err)
-        }
-        log('Listening at localhost:3000');
-    });
 
-}
+app.get('/', function (req, res) {
+    log('URL: ', req.url)
+    match({ routes, location: req.url }, (error, redirectLocation, renderProps) => {
+        log('ERROR: ', error)
+        log('RENDER PROPS: ', renderProps)
+        if (error) {
+            res.status(500).send(error.message)
+        } else if (redirectLocation) {
+            res.redirect(302, redirectLocation.pathname + redirectLocation.search)
+        } else if (renderProps) {
+            res.status(200).send(renderPage(renderProps, true))
+        } else {
+            res.status(404).send('Not found hihi')
+        }
+    })
+});
+
+app.listen(process.env.PORT || 3000, 'localhost', function (err, result) {
+    if (err) {
+        console.error(err);
+        log(err)
+    }
+    log('Listening at localhost:3000');
+});
+
